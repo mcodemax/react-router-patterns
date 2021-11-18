e7d709d40cf9150015af47067c296e7cac6fdf9d
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2021-11-18 08:17:49 -0800

    Create Nav.js

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+// import './Nav.css'; //style later .active classes
+
+
+const Nav = ({dogs}) => {
+  return (
+      <nav>
+        {
+          dogs.map(dog => {
+            return (
+              <NavLink exact={"true"} to={`${dog.name}`}>Chips</NavLink>
+            )
+          })
+        }
+      </nav>
+  );
+};
+
+export default Nav;
